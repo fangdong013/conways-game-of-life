3189a9b69e9991e20a3cc7793ffb8354a3322da8
Kacper <kacper@cygan.com>
2017-01-23 15:22:47 +0100

    git error correction

--- conways-game-of-life/conway.m
+++ conways-game-of-life/conway.m
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function [outputMatrix] = conway(inputMatrix)
 
 % A = randi([0 1], 100, 100);
@@ -9,16 +8,4 @@ function [outputMatrix] = conway(inputMatrix)
     outputMatrix = nextMat(inputMatrix, checkMat(inputMatrix));
 end
     
-=======
-function [] = conway(matrix)
 
-A = matrix;
-
-
-gridGenerator(A)
-pause(0.0001)
-A = nextMat(A, checkMat(A));
-   
-end
-    
->>>>>>> origin/master
